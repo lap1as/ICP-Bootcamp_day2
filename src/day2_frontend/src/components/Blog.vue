b1ef6e871cf77b0056631deea94ed40cde18fbb9
lap1as <kuzukoleg12@gmail.com>
2024-06-25 19:58:03 +0200

    add addEntries method to Blog

--- src/day2_frontend/src/components/Blog.vue
+++ src/day2_frontend/src/components/Blog.vue
@@ -1,5 +1,29 @@
-<<template>
+<template>
     <div>
+        <button @click="getEnties">Get Entries</button>
         Hi blog!
+        {{ entries }}
+        <input v-model="newBlog" type="text">
+        <button @click="addEntries">Add Entries</button>
     </div>
 </template>
+
+<script>
+import { day2_backend } from 'declarations/day2_backend/index';
+
+export default {
+    data() {
+        return {
+            entries: []
+        }
+    },
+    methods: {
+        async getEnties() {
+            this.entries = await day2_backend.get_entries();
+        },
+        async addEntries(){
+            await day2_backend.add_entry(this.newBlog);
+        }
+    },
+}
+</script>
