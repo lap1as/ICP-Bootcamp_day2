6bb128a82c7fd53463d3aeac742f86cee9f0b599
lap1as <kuzukoleg12@gmail.com>
2024-06-25 19:13:09 +0200

    add and get entries

--- src/day2_backend/src/lib.rs
+++ src/day2_backend/src/lib.rs
@@ -1,4 +1,24 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static ENTRIES: RefCell<Vec<String>> = RefCell::default();
+}
+
 #[ic_cdk::query]
 fn greet(name: String, last_name: i8) -> String {
     format!("Hello, {} {}!", name, last_name)
+}
+
+#[ic_cdk::update]
+fn add_entry(entry: String) {
+    ENTRIES.with(|entries| {
+        entries.borrow_mut().push(entry)
+    });
+}
+
+#[ic_cdk::query]
+fn get_entries() -> Vec<String> {
+    ENTRIES.with(|entries|{
+        entries.borrow().clone()
+    })
 }
